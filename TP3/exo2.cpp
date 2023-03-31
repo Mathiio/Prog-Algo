f9c92411fa089c1c3b94e4cc7ded6c13e13d1c47
Mathiio <matheo.pougalan@gmail.com>
2023-03-31 12:14:10 +0200

    add: comment TP3 ex2

--- TP3/exo2.cpp
+++ TP3/exo2.cpp
@@ -15,47 +15,70 @@ using std::size_t;
 
 void binarySearchAll(Array& array, int toSearch, int& indexMin, int& indexMax)
 {
+    // Initialise indice bas pour recherche binaire
     int low = 0;
+    // Initialise indice haut pour recherche binaire
     int high = array.size() - 1;
+    // Initialise indice milieu pour recherche binaire
     int mid = (low + high) / 2;
+    // Initialise booléen -> true si élément recherché trouvé
     int found = 0;
 
-    // Binary search for min index
+
+    // Tant que low inférieur à taille totale tableau
     while (low <= high) {
+        // Si élément au milieu est inférieur à l'élément recherché
         if (array[mid] < toSearch) {
+            // Déplace indice bas pour ignorer moitié gauche
             low = mid + 1;
+            // Si élément milieu égal à élément cherché
         } else if (array[mid] == toSearch) {
+            // Définit indice min sur indice milieu actuel
             indexMin = mid;
+            // Déplace indice haut pour ignorer moitié droite
             high = mid - 1;
+            // Passe found à true car élément recherché trouvé
             found = 1;
+            // Si élément milieu est supérieur à élément recherché
         } else {
+            // Déplace indice haut pour ignorer moitié droite
             high = mid - 1;
         }
-
+        // Réinitialise indice milieu
         mid = (low + high) / 2;
     }
 
-    // If find no result
+    // Si pas résultat pour find
     if (!found) {
+        // Passe indexMin et indexMax à -1
         indexMin = indexMax = -1;
         return;
     }
 
-    // Binary search for max index
+    // Recherche binaire pour indice max
+    // Réinitialise tous les indices
     low = indexMin;
     high = array.size() - 1;
     mid = (low + high) / 2;
 
+    // Tant que low inférieur à taille totale tableau
     while (low <= high) {
+        // Si élément milieu est supérieur élément recherché
         if (array[mid] > toSearch) {
+            // Déplace indice haut pour ignorer moitié droite
             high = mid - 1;
+            // Si élément milieu est égal à élément recherché
         } else if (array[mid] == toSearch) {
+            // Définit indice max sur indice milieu actuel
             indexMax = mid;
+            // Déplace indice bas pour ignorer moitié gauche
             low = mid + 1;
+            // Si élément milieu est supérieur à élément recherché
         } else {
+            // Déplace indice bas pour ignorer moitié gauche
             low = mid + 1;
         }
-
+        // Réinitialise indice milieu
         mid = (low + high) / 2;
     }
 }
